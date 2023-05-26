a84468bbaba318fafd77fbdb5a248c02ea47c033
Julio <alvarez.julio322@gmail.com>
2023-05-25 20:46:03 -0400

    updated install and database js files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,19 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visiblity = 'visible';
+    butInstall.textContent = 'install'
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'installed';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appisinstalled', (event) => {
+    console.log('appisinstalled', event);
+})
